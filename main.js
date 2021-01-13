35a5512ecea6c42b3ab387f5406ed0669fdee670
Grace Snow <grace.snow@mandogroup.com>
2021-01-13 15:41:31 +0000

    event listeners

--- main.js
+++ main.js
@@ -11,5 +11,24 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    }, 
+    methods: {
+      addToCart() {
+        const btn = document.querySelector('.js-remove');
+        this.cart += 1;
+        btn.removeAttribute('disabled');
+      },
+      removeFromCart() {
+        const btn = document.querySelector('.js-remove');
+        if (this.cart > 0) {
+          this.cart -= 1
+        }
+        if (this.cart === 0) {
+          btn.setAttribute('disabled', '');
+        }
+      },
+      updateImage(variantImage) {
+        this.image = variantImage;
+      }
     }
 })
