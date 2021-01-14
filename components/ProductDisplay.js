8be59731f6e0ee2be4fa6a07475215320693ab30
Grace Snow <grace.snow@mandogroup.com>
2021-01-14 17:13:16 +0000

    forms and v-model

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -6,42 +6,47 @@ app.component('product-display', {
     }
   },
   template: 
-  /*html*/
-  `<div class="product-display">
-    <div class="product-container">
-      <div class="product-image">
-        <img v-bind:src="image">
-      </div>
-      <div class="product-info">
-        <h1>{{ title }}</h1>
-
-        <p v-if="inStock">In Stock</p>
-        <p v-else>Out of Stock</p>
-
-        <p>Shipping: {{ shipping }}</p>
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-
-        <div 
-          v-for="(variant, index) in variants" 
-          :key="variant.id" 
-          @mouseover="updateVariant(index)" 
-          class="color-circle" 
-          :style="{ backgroundColor: variant.color }">
+    /*html*/
+    `<div class="product-display">
+      <div class="product-container">
+        <div class="product-image">
+          <img v-bind:src="image">
         </div>
-        
-        <button 
-          class="button" 
-          :class="{ disabledButton: !inStock }" 
-          :disabled="!inStock" 
-          v-on:click="addToCart">
-          Add to Cart
-        </button>
+        <div class="product-info">
+          <h1>{{ title }}</h1>
 
+          <p v-if="inStock">In Stock</p>
+          <p v-else>Out of Stock</p>
+
+          <p>Shipping: {{ shipping }}</p>
+          <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+          </ul>
+
+          <div 
+            v-for="(variant, index) in variants" 
+            :key="variant.id" 
+            @mouseover="updateVariant(index)" 
+            class="color-circle" 
+            :style="{ backgroundColor: variant.color }">
+          </div>
+          
+          <button 
+            class="button" 
+            :class="{ disabledButton: !inStock }" 
+            :disabled="!inStock" 
+            v-on:click="addToCart">
+            Add to Cart
+          </button>
+        </div>
+          
       </div>
-    </div>
-  </div>`,
+      
+      <review-list v-if="reviews.length " :reviews="reviews"></review-list>
+      
+      <review-form @review-submitted="addReview"></review-form>
+    </div>`
+  ,
   data() {
     return {
         product: 'Socks',
@@ -51,7 +56,8 @@ app.component('product-display', {
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
           { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-        ]
+        ],
+        reviews: [],
     }
   },
   methods: {
@@ -60,6 +66,9 @@ app.component('product-display', {
       },
       updateVariant(index) {
           this.selectedVariant = index
+      },
+      addReview(review) {
+        this.reviews.push(review);
       }
   },
   computed: {
---
+++ components/ReviewForm.js
@@ -0,0 +1,57 @@
+app.component('review-form', {
+  template:
+    /* html */
+    `<form class="review-form" @submit.prevent="onSubmit">
+    <!--/* .prevent on the submit is a modifier method that prevents the default */-->
+    <h3>Leave a review</h3>
+    
+    <label for="name">Name:</label>
+    <input id="name" v-model="name" type="text">
+    
+    <label for="review">Review:</label>      
+    <textarea id="review" v-model="review"></textarea>
+    
+    <label for="rating">Rating:</label>
+    <!--/* v-model.number - number is a modifier method that typecasts the data as number */-->
+    <select id="rating" v-model.number="rating">
+      <option>5</option>
+      <option>4</option>
+      <option>3</option>
+      <option>2</option>
+      <option>1</option>
+    </select>
+    
+    <input class="button" type="submit" value="Submit">  
+  </form>`,
+  data() {
+    return {
+      name: '',
+      review: '',
+      rating: null,
+    }
+  }, 
+  methods: {
+    onSubmit() {
+      // 4. add completion check
+      if (this.name === '' || this.review === '' || this.rating === null) {
+        alert('Please fill out all fields');
+        return
+      }
+
+      // 1. capture form data - this will be the payload
+      let productReview = {
+        name: this.name,
+        review: this.review,
+        rating: this.rating,
+      }
+
+      // 2. emit that data so it can live on the product, not this form
+      this.$emit('review-submitted', productReview);
+
+      // 3. Reset form data
+      this.name   = '';
+      this.review = '';
+      this.rating = null;
+    }
+  },
+})
